d1cbcf4890704fcd48e22a264eba669816f2938c
Devin Cooper <devinraycooper@gmail.com>
2020-01-05 01:33:25 -0500

    finish objective 2 lab 2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,14 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Charles Peterson";
+    int age = 47;
+    String state = "Arkansas";
+
+    name = "Devin Cooper";
+    age = 34;
+    state = "Florida";
+
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+
+  }
+}
