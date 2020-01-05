6800acc6a747dcb5235a62a2040371b46f5ae74d
Devin Cooper <devinraycooper@gmail.com>
2020-01-04 19:00:38 -0500

    finish objective 1 lab 2

--- Labs/Objective1Lab2.java
+++ Labs/Objective1Lab2.java
@@ -1,6 +1,7 @@
-public class Objective1Lab1 {
+public class Objective1Lab2 {
   public static void main(String[] args) {
-
     System.out.println("Devin Cooper");
+    System.out.println("34");
+
   }
 }
