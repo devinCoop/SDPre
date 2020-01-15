1dedd6e9fa3a4ba047ad9f4865e29d78d46091fb
Devin Cooper <devinraycooper@gmail.com>
2020-01-15 10:55:16 -0700

    finish objective 4 lab 3

---
+++ Labs/Objective4Lab3.java
@@ -0,0 +1,23 @@
+import static java.lang.System.*;
+import java.util.Scanner;
+
+public class Objective4Lab3 {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    int userNum;
+
+    out.println("Please enter a number: ");
+
+    userNum = scanner.nextInt();
+
+    if (userNum > 0) {
+      out.println("The number is positive");
+    }
+    else if (userNum == 0) {
+      out.println("The number is 0");
+    }
+    else if (userNum < 0) {
+      out.println("The number is negative");
+    }
+  }
+}
