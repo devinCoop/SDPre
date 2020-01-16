473c3ee2a47652b4f5ab40fbe1213dd3fb08f2b8
Devin Cooper <devinraycooper@gmail.com>
2020-01-15 17:45:03 -0700

    finish objective 6 lab 2

---
+++ Labs/Objective6Lab2.java
@@ -0,0 +1,12 @@
+import static java.lang.System.*;
+
+public class Objective6Lab2 {
+  public static void main(String[] args) {
+    int counter = 0;
+
+      while (counter <= 20) {
+        out.println(counter);
+        counter = counter + 1;
+      }
+  }
+}
