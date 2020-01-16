fbe8d82e3822f2593147ee4e4a8220edceca8ea5
Devin Cooper <devinraycooper@gmail.com>
2020-01-15 18:07:00 -0700

    finish objective 6 lab 4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,17 @@
+import static java.lang.System.*;
+
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int sumTotal = 0, counter = 1;
+
+    while (counter <= 20) {
+
+      sumTotal += counter;
+      counter = counter + 1;
+
+    }
+
+    out.println(sumTotal);
+
+  }
+}
